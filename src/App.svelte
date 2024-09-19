fdc41398e157e3d48f64846998422f395634dce0
Matheus Torreão <mtorreao1@gmail.com>
2024-09-19 08:25:19 -0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<main>
+  <h1 class="text-3xl font-bold">
+    Hello world!
+  </h1>
+</main>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.900);
+  }
+</style>
